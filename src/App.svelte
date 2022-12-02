11495d062f02e2615a8b8117505cc5a9ce0cca08
SerhiiShe <111283858+SerhiiShe@users.noreply.github.com>
2022-12-02 10:53:38 +0200

    update

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import TodoList from './TodoList.svelte';
+</script>
+
+<main>
+	<TodoList/>
+</main>
+
+<style>
+	main {
+		width: 100%;
+		height: 100vh;
+		min-width: 500px;
+		min-height: 500px;
+		overflow: hidden;
+	}
+</style>
